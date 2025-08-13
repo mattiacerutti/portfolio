d5ae4ed907d9c0a52c52ba645ece2cb1fab34dc6
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-08-13 21:11:35 +0100

    feat: add system theme option in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,33 @@
 "use client";
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {FiMoon} from "react-icons/fi";
 import {FiSun} from "react-icons/fi";
+import {RiComputerLine} from "react-icons/ri";
 import {useTheme} from "next-themes";
 
 export default function Footer() {
-  const { theme, setTheme } = useTheme();
-  const [isMounted, setIsMounted] = React.useState(false);
-  React.useEffect(() => {
+  const {setTheme, resolvedTheme} = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
     setIsMounted(true);
   }, []);
-  const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
+
+  const toggleTheme = () => setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  const setSystemTheme = () => setTheme("system");
   return (
     <footer className="flex items-center justify-between w-full">
       <p className="text-base text-gray-500">&copy; {new Date().getFullYear()}</p>
       {isMounted && (
-        <button className="text-gray-500 transition cursor-pointer hover:text-[var(--link-hover)] duration-400" onClick={toggleTheme}>
-          {theme === "dark" ? <FiSun className="w-5 h-5" /> : <FiMoon className="w-5 h-5" />}
-        </button>
+        <div className="flex items-center justify-end gap-4">
+          <button className="text-gray-500 transition cursor-pointer hover:text-[var(--link-hover)] duration-400" onClick={setSystemTheme}>
+            <RiComputerLine className="w-5 h-5" />
+          </button>
+          <button className="text-gray-500 transition cursor-pointer hover:text-[var(--link-hover)] duration-400" onClick={toggleTheme}>
+            {resolvedTheme === "dark" ? <FiSun className="w-5 h-5" /> : <FiMoon className="w-5 h-5" />}
+          </button>
+        </div>
       )}
     </footer>
   );
