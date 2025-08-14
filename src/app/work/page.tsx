b386af036d4b7b969f1ca4e59a0a1a1a69e94129
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-08-14 21:41:48 +0100

    fix: add paragraph to work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,6 +3,7 @@ import H3 from "@/components/typography/h3";
 import WorkExperience from "@/components/work-experience";
 import {WORK_EXPERIENCES} from "@/data/work";
 import VerticalReveal from "@/components/animations/vertical-reveal";
+import P from "@/components/typography/p";
 
 export default function Work() {
   const baseDelay = 0.15;
@@ -11,6 +12,7 @@ export default function Work() {
     <div className="flex flex-col gap-15">
       <VerticalReveal>
         <H3>Work</H3>
+        <P className="text-[var(--link-text)]">My work experiences.</P>
       </VerticalReveal>
       <div className="flex flex-col gap-10">
         {WORK_EXPERIENCES.map((experience, index) => (
