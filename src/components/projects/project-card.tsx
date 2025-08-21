ec179b4e66b816dde9a2a38854a47d6836e9ea35
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-08-21 19:33:02 +0100

    fix: fix small styling bug

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -16,7 +16,7 @@ function ProjectCard(props: IProjectCardProps) {
   return (
   <CustomLink href={`/projects/${id}`} className="cursor-pointer">
       <div className="p-4 rounded-xl border-[0.08rem] border-[var(--foreground)]/10 bg-[var(--foreground)]/1 hover:bg-[var(--foreground)]/4 transition-colors duration-400">
-        <H4>{title}</H4>
+        <H4 className="text-[var(--foreground)]">{title}</H4>
         <P className="text-[var(--muted-foreground)] font-extralight">{description}</P>
         {technologies && (
           <div className="flex flex-wrap mt-5 gap-2">
