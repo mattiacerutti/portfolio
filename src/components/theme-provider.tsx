1cbb997dfb1a3a6018e6ec114de77ad056391dd4
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-08-22 23:52:33 +0100

    fix: fix overscroll background not changing with theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { ThemeProvider as NextThemeProvider } from "next-themes";
+import {ThemeProvider as NextThemeProvider} from "next-themes";
 import React from "react";
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
+export function ThemeProvider({children}: {children: React.ReactNode}) {
   return (
-    <NextThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
+    <NextThemeProvider attribute="data-theme" defaultTheme="system" enableSystem themeColor={"var(--background)"}>
       {children}
     </NextThemeProvider>
   );
