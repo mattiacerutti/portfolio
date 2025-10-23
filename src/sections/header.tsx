d23b594fe080943db65d6251c4ee7eac08ae8221
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-10-23 11:59:17 +0200

    fix: make header name white

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 z-50 w-full p-6 text-xl border-b bg-[var(--foreground)]/2 border-gray-500/10 bg-clip-padding backdrop-blur-md">
       <div className="flex justify-between max-w-4xl mx-auto">
-        <CustomLink href="/" className="font-semibold">
+        <CustomLink href="/" className="font-semibold !text-[var(--foreground)]/80">
           Mattia Cerutti
         </CustomLink>
         <NavLinks />
